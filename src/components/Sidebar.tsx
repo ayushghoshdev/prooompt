2c2d7b5c73a49c1c0a23e00484ecbd622b5ec3ce
ayushghoshdev <ghoshayush2008@gmail.com>
2026-05-03 23:34:43 +0530

    Update Sidebar.tsx (add collapsing)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { PlusIcon } from "@phosphor-icons/react";
+import { CaretRightIcon, PlusIcon } from "@phosphor-icons/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState, useRef, useCallback, useEffect } from "react";
@@ -8,6 +8,7 @@ import { useState, useRef, useCallback, useEffect } from "react";
 export default function Sidebar() {
   const [width, setWidth] = useState(300);
   const [isDragging, setIsDragging] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
   const sidebarRef = useRef<HTMLDivElement>(null);
 
   const onMouseDown = () => {
@@ -60,16 +61,38 @@ export default function Sidebar() {
             className="transition-transform duration-1000 ease-in-out hover:rotate-360"
           />
         </Link>
-        <div className="flex items-center justify-between w-full bg-secondary text-muted-foreground rounded-lg mt-4 px-4 py-2">
-          <p className="font-medium">Chats</p>
+        <div className="w-full bg-secondary text-muted-foreground rounded-lg mt-4 px-4 py-2">
+          <div className="flex items-center justify-between">
+            <p className="font-medium">Chats</p>
 
-          <Link href="/">
-            <PlusIcon
-              size={16}
-              weight="bold"
-              className="hover:text-foreground"
-            />
-          </Link>
+            <div className="flex gap-2">
+              <Link href="/">
+                <PlusIcon
+                  size={16}
+                  weight="bold"
+                  className="hover:text-foreground"
+                />
+              </Link>
+              <CaretRightIcon
+                size={16}
+                weight="bold"
+                onClick={() => setIsCollapsed(!isCollapsed)}
+                className={`cursor-pointer hover:text-foreground -mr-1 transition-transform duration-300 ${isCollapsed ? "rotate-90" : "rotate-0"}`}
+              />
+            </div>
+          </div>
+
+          <div
+            className={`grid transition-[grid-template-rows] duration-300 ease-in-out ${
+              isCollapsed ? "grid-rows-[0fr]" : "grid-rows-[1fr]"
+            }`}
+          >
+            <div
+              className={`overflow-hidden transition-opacity duration-300 ${isCollapsed ? "opacity-0" : "opacity-100"}`}
+            >
+              <p className="mt-2 text-muted-foreground/60">No Chats yet.</p>
+            </div>
+          </div>
         </div>
         <div
           onMouseDown={onMouseDown}
