b2f8a17a3c8d11f674ee1ca0bb8c24f2c26f0287
Ayush Ghosh <ghoshayush2008@gmail.com>
2026-05-04 22:15:35 +0530

    Make sidebar collapsible

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,20 @@
 "use client";
 
-import { CaretRightIcon, PlusIcon } from "@phosphor-icons/react";
+import {
+  CaretRightIcon,
+  PlusIcon,
+  SidebarSimpleIcon,
+} from "@phosphor-icons/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState, useRef, useCallback, useEffect } from "react";
+import { Button } from "./ui/button";
 
 export default function Sidebar() {
   const [width, setWidth] = useState(300);
   const [isDragging, setIsDragging] = useState(false);
-  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+  const [isChatsCollapsed, setIsChatsCollapsed] = useState(false);
   const sidebarRef = useRef<HTMLDivElement>(null);
 
   const onMouseDown = () => {
@@ -49,55 +55,78 @@ export default function Sidebar() {
     <>
       <div
         ref={sidebarRef}
-        style={{ width: `${width}px` }}
-        className="h-screen text-white p-4 relative"
+        style={!isSidebarCollapsed ? { width: `${width}px` } : {}}
+        className={
+          isSidebarCollapsed
+            ? "fixed top-4 left-4 z-50 flex items-center gap-4 text-white"
+            : "h-screen text-white p-4 relative"
+        }
       >
-        <Link href="/">
-          <Image
-            src="/icon.png"
-            width={30}
-            height={30}
-            alt="Icon"
-            className="transition-transform duration-1000 ease-in-out hover:rotate-360"
-          />
-        </Link>
-        <div className="w-full bg-secondary text-muted-foreground rounded-lg mt-4 px-4 py-2">
-          <div className="flex items-center justify-between">
-            <p className="font-medium">Chats</p>
-
-            <div className="flex gap-2">
-              <Link href="/">
-                <PlusIcon
-                  size={16}
-                  weight="bold"
-                  className="hover:text-foreground"
-                />
-              </Link>
-              <CaretRightIcon
-                size={16}
-                weight="bold"
-                onClick={() => setIsCollapsed(!isCollapsed)}
-                className={`cursor-pointer hover:text-foreground -mr-1 transition-transform duration-300 ${isCollapsed ? "rotate-90" : "rotate-0"}`}
-              />
-            </div>
-          </div>
-
-          <div
-            className={`grid transition-[grid-template-rows] duration-300 ease-in-out ${
-              isCollapsed ? "grid-rows-[0fr]" : "grid-rows-[1fr]"
-            }`}
-          >
-            <div
-              className={`overflow-hidden transition-opacity duration-300 ${isCollapsed ? "opacity-0" : "opacity-100"}`}
-            >
-              <p className="mt-2 text-muted-foreground/60">No Chats yet.</p>
-            </div>
-          </div>
-        </div>
         <div
-          onMouseDown={onMouseDown}
-          className="absolute top-0 right-0 h-full border-r-3 border-dashed border-muted cursor-col-resize hover:border-secondary-foreground"
-        />
+          className={
+            isSidebarCollapsed
+              ? "flex items-center gap-2"
+              : "flex items-center justify-between"
+          }
+        >
+          <Link href="/">
+            <Image
+              src="/icon.png"
+              width={30}
+              height={30}
+              alt="Icon"
+              className="transition-transform duration-1000 ease-in-out hover:rotate-360"
+            />
+          </Link>
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => setIsSidebarCollapsed(!isSidebarCollapsed)}
+          >
+            <SidebarSimpleIcon weight="bold" className="size-5" />
+          </Button>
+        </div>
+        {!isSidebarCollapsed && (
+          <>
+            <div className="w-full bg-secondary text-muted-foreground rounded-lg mt-4 px-4 py-2">
+              <div className="flex items-center justify-between">
+                <p className="font-medium">Chats</p>
+
+                <div className="flex gap-2">
+                  <Link href="/">
+                    <PlusIcon
+                      size={16}
+                      weight="bold"
+                      className="hover:text-foreground"
+                    />
+                  </Link>
+                  <CaretRightIcon
+                    size={16}
+                    weight="bold"
+                    onClick={() => setIsChatsCollapsed(!isChatsCollapsed)}
+                    className={`cursor-pointer hover:text-foreground -mr-1 transition-transform duration-300 ${isChatsCollapsed ? "rotate-90" : "rotate-0"}`}
+                  />
+                </div>
+              </div>
+
+              <div
+                className={`grid transition-[grid-template-rows] duration-300 ease-in-out ${
+                  isChatsCollapsed ? "grid-rows-[0fr]" : "grid-rows-[1fr]"
+                }`}
+              >
+                <div
+                  className={`overflow-hidden transition-opacity duration-300 ${isChatsCollapsed ? "opacity-0" : "opacity-100"}`}
+                >
+                  <p className="mt-2 text-muted-foreground/60">No Chats yet.</p>
+                </div>
+              </div>
+            </div>
+            <div
+              onMouseDown={onMouseDown}
+              className="absolute top-0 right-0 h-full border-r-3 border-dashed border-muted cursor-col-resize hover:border-secondary-foreground"
+            />
+          </>
+        )}
       </div>
     </>
   );
